4bac533405d618a1d7e33370770c5270eeb84a73
saim538 <saimgraphics001@gmail.com>
2025-03-14 11:04:40 +0500

    Update schematic.py

--- schematic.py
+++ schematic.py
@@ -161,7 +161,7 @@ if st.button("Fetch Data"):
                 subs = int(channel["statistics"].get("subscriberCount", 0))
 
 
-                if subs < 5000:  # Only include channels with fewer than 3,000 subscribers
+                if subs < 20000:  # Only include channels with fewer than 3,000 subscribers
 
                     all_results.append({
 
